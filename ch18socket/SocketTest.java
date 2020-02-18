d50bfa0fcac4a86039ce87a56a9e5eda1d35c33c
yanxuanshaozhu <973493269@qq.com>
2020-02-18 22:37:53 +0800

    Upload files

---
+++ ch18socket/SocketTest.java
@@ -0,0 +1,27 @@
+package pers.yanxuanshaozhu.corejavach18.ch18socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketTest {
+	public static void main(String[] args) throws IOException {
+		try (Socket socket = new Socket("time-a.nist.gov", 13);
+				Scanner in = new Scanner(socket.getInputStream(), "UTF-8")) {
+			while (in.hasNextLine()) {
+				String line = in.nextLine();
+				System.out.println(line);
+			}
+		}
+
+		try (Socket socket = new Socket("time-a.nist.gov", 13);
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+			while (bufferedReader.readLine() != null) {
+				String line = bufferedReader.readLine();
+				System.out.println(line);
+			}
+		}
+	}
+}
